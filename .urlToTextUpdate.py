7d3fca50b4ef36501335507650fa119de477511f
waLLxAck <svilen.petrov97@gmail.com>
2020-09-11 12:40:54 +0100

    intial commit

---
+++ .urlToTextUpdate.py
@@ -0,0 +1,15 @@
+import glob
+
+arrayFileNames = glob.glob("*.url")
+strUrl = ""
+
+for x in arrayFileNames:
+    with open(x, "r") as infile:
+        for line in infile:
+            if (line.startswith('URL')):
+                strUrl += "File Name: "+ x[:-4] + "\nURL: " + line[4:] +"\n"
+                break
+
+text_file = open("Name and URL.txt", "a")
+n = text_file.write(strUrl)
+text_file.close()
